7d6e97d18ac6f0360f1323e513dd9cfd4352578a
Alwin-Aerospace <alwin.j.aerospace@gmail.com>
2025-09-23 03:52:12 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var button =document.getElementById("btn")
+
+function change()
+{
+    window.location.href="skills.html"
+}
+
+var button=document.getElementById("btn1")
+function change1()
+{
+   window.location.href="project.html"
+}
